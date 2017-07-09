a9ca9595ab6c56c324e553cae104966a839096e4
aj1155 <aj1155@naver.com>
2017-07-09 22:40:11 +0900

    [api] api 프로젝트 구성 추가

---
+++ src/main/java/me/nolza/NolzaApplication.java
@@ -0,0 +1,12 @@
+package me.nolza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NolzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NolzaApplication.class, args);
+	}
+}
