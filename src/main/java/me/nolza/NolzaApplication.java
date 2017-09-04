52f9b1999246974112fd3b08ea51b66e7608750c
GAIN LEE <gain@GAINui-MacBook-Pro.local>
2017-09-04 20:24:11 +0900

    [api] mysql 서버 설정 정보 수정

--- src/main/java/me/nolza/NolzaApplication.java
+++ src/main/java/me/nolza/NolzaApplication.java
@@ -2,9 +2,16 @@ package me.nolza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NolzaApplication {
+public class NolzaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NolzaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NolzaApplication.class, args);
